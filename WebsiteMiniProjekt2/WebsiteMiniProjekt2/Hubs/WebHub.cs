d92e9cad5156cd63095a8bd198e3d138001b564e
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2022-11-23 16:33:15 +0100

    ... det virker ikke

---
+++ WebsiteMiniProjekt2/WebsiteMiniProjekt2/Hubs/WebHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace WebsiteMiniProjekt2.Hubs
+{
+    public class WebHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task verifyCode(string code)
+        {
+            await Clients.All.SendAsync("isYourCode", code);
+            Trace.WriteLine(code);
+            //all clients should have a listener for isYourCode, that sends back
+            //the code that they use to everyone.
+        }
+    }
+}
--- WebsiteMiniProjekt2/WebsiteMiniProjekt2/Program.cs
+++ WebsiteMiniProjekt2/WebsiteMiniProjekt2/Program.cs
@@ -1,7 +1,11 @@
+using WebsiteMiniProjekt2.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -21,5 +25,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<WebHub>("/webHub");
 
 app.Run();
