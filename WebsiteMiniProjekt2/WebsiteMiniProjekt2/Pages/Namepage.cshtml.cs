6332273a907c9c87700b8f72f58600be4b576e3d
Jhohnhahthhahnh <cathrine@duedahl-olesen.dk>
2022-11-23 16:33:29 +0100

    Multiple choice og videreførende sider

---
+++ WebsiteMiniProjekt2/WebsiteMiniProjekt2/Pages/Namepage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteMiniProjekt2.Pages
+{
+    public class NamepageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
