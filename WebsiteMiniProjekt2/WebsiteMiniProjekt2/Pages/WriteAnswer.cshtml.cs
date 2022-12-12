e355318f7be1a4d8700f3627dca125e23284a336
Jhohnhahthhahnh <cathrine@duedahl-olesen.dk>
2022-12-12 16:32:45 +0100

    Oprettelse og færdiggørelse af diverse sider

---
+++ WebsiteMiniProjekt2/WebsiteMiniProjekt2/Pages/WriteAnswer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteMiniProjekt2.Pages.Shared
+{
+    public class WriteAnswerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
