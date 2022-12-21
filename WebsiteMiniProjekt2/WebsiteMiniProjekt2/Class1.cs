7f20e2c139831354856e1d95c7f54105c202aca5
ZeBoogie <jonatan.d.falk@gmail.com>
2022-12-21 15:48:11 +0100

    JSON istedet for SQLite

--- WebsiteMiniProjekt2/WebsiteMiniProjekt2/Class1.cs
+++ WebsiteMiniProjekt2/WebsiteMiniProjekt2/Class1.cs
@@ -1,16 +1,6 @@
-﻿using Microsoft.Data.Sqlite;
-
-namespace WebsiteMiniProjekt2
+﻿namespace WebsiteMiniProjekt2
 {
-    public class Database
+    public class Class1
     {
-        public SqliteConnection myConnecntion;
-
-        public Database() 
-        {
-            myConnecntion = new SqliteConnection
-                ("Data Source=C:\\Users\\Jonat\\Documents\\HCØ\\3.G\\Teknik - DDU\\databaser\\mydatabase.sqlite");
-        }
-
     }
 }
